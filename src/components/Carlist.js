d2a603e02cc45b756083e5194e8e5f309f2dce16
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2023-03-19 11:55:22 +0100

    frontend to car api

---
+++ src/components/Carlist.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+
+export default function Carlist() {
+
+    const [cars, setCars] = useState([]);
+
+    useEffect(
+        () => {fetch('http://localhost:8080/api/cars')
+        .then(response => response.json())
+        .then(data => setCars(data._embedded.cars))
+        .catch(err => console.error(err));
+        }, []);
+
+
+    return(
+        <div>
+            <table className="Table">
+                <tbody className="TabBody">
+                {
+                cars.map((car, index) =>
+                    <tr key={index}>
+                    <td>{car.brand}</td>
+                    <td>{car.model}</td>
+                    <td>{car.color}</td>
+                    <td>{car.year}</td>
+                    <td>{car.price}</td>
+                    </tr>)
+                }
+                </tbody>
+            </table>
+        </div>
+    );
+}
